582f6529ddce6761a442d813b08ddd408de71165
csterling <coreytsterling@gmail.com>
2022-01-27 17:03:53 +1300

    Added formatter for including inputs/outputs of contracts.

--- src/ufdl/jobcontracts/util/__init__.py
+++ src/ufdl/jobcontracts/util/__init__.py
@@ -1,2 +1,2 @@
-from ._format import format_contract_type
+from ._format import format_contract_type, format_contract
 from ._parse import parse_contract
--- src/ufdl/jobcontracts/util/_format.py
+++ src/ufdl/jobcontracts/util/_format.py
@@ -1,10 +1,44 @@
 from typing import Type
 
+from ufdl.jobtypes.util import format_type
+
 from ..base import UFDLJobContract
 from ..initialise import name_type_translate
 
 
-def format_contract_type(contract_type: Type[UFDLJobContract]) -> str:
+def format_contract_type(
+        contract_type: Type[UFDLJobContract],
+        *,
+        include_io: bool = False
+) -> str:
     if contract_type is UFDLJobContract:
         raise Exception(f"Can't format base contract class {UFDLJobContract.__qualname__}")
-    return f"{name_type_translate(contract_type)}{contract_type.params()}"
+
+    contract_name = name_type_translate(contract_type)
+    contract_params = contract_type.params()
+
+    if not include_io:
+        return f"{contract_name}{contract_params}"
+
+    raise NotImplementedError()
+
+
+def format_contract(
+        contract: UFDLJobContract,
+        *,
+        include_io: bool = False
+) -> str:
+    if not include_io:
+        return str(contract)
+
+    inputs = ",\n".join(
+        f"\t{input_name}: " + " | ".join(map(format_type, input_types))
+        for input_name, input_types in contract.inputs.items()
+    )
+
+    outputs = ",\n".join(
+        f"\t{output_name}: {format_type(output_type)}"
+        for output_name, output_type in contract.outputs.items()
+    )
+
+    return f"{contract}(\n{inputs}\n) -> {{\n{outputs}\n}}"
