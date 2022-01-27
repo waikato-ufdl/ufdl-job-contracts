44d825ef20975ac294520ba15795d9faaaa8261b
csterling <coreytsterling@gmail.com>
2022-01-27 17:04:19 +1300

    Predict can now take a dataset by name.

--- src/ufdl/jobcontracts/standard/_Predict.py
+++ src/ufdl/jobcontracts/standard/_Predict.py
@@ -1,18 +1,27 @@
-from ufdl.jobtypes.standard import JobOutput, Model, PK
+from ufdl.jobtypes.standard import JobOutput, Model, PK, Name
 from ufdl.jobtypes.standard.server import Dataset, Domain, Framework
 
 from ..base import UFDLJobContract
 from ..params import JobContractParams, TypeConstructor
 
+# Type parameters
 predict_params = JobContractParams()
 DomainType = predict_params.add_simple_param('DomainType', Domain)
 FrameworkType = predict_params.add_simple_param('FrameworkType', Framework)
-ModelType = predict_params.add_dependent_param('ModelType', Model, DomainType, FrameworkType)
 
-model_type_constructor = TypeConstructor.indirect_dependency(JobOutput, ModelType)
+# IO type constructors
+model_type_constructor = TypeConstructor.indirect_dependency(
+    JobOutput,
+    TypeConstructor.indirect_dependency(Model, DomainType, FrameworkType)
+)
+dataset_type_constructor = TypeConstructor.indirect_dependency(Dataset, DomainType)
 dataset_pk_type_constructor = TypeConstructor.indirect_dependency(
     PK,
-    TypeConstructor.indirect_dependency(Dataset, DomainType)
+    dataset_type_constructor
+)
+dataset_name_type_constructor = TypeConstructor.indirect_dependency(
+    Name,
+    dataset_type_constructor
 )
 
 
@@ -21,7 +30,7 @@ class Predict(
     params=predict_params,
     inputs={
         "model": (model_type_constructor,),
-        "dataset": (dataset_pk_type_constructor,)
+        "dataset": (dataset_pk_type_constructor, dataset_name_type_constructor)
     },
     outputs={
         "predictions": dataset_pk_type_constructor
