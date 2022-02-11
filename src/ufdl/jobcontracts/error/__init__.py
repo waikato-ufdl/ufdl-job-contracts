03ebedcf5e4b55f90d654e561faaeda75465b75b
csterling <coreytsterling@gmail.com>
2022-02-11 14:14:46 +1300

    Remove expect in favour of ufdl-job-types implementation.

--- src/ufdl/jobcontracts/error/__init__.py
+++ src/ufdl/jobcontracts/error/__init__.py
@@ -1,3 +1,2 @@
 from ._ContractParsingException import ContractParsingException
-from ._ExpectationError import ExpectationError, expect
 from ._UnknownContractNameException import UnknownContractNameException
